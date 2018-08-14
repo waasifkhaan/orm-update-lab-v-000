5782094b0c5ae2e580020f6f560f3cfd8cc1a312
Wasif Khan <waasif.khaan@gmail.com>
2018-08-14 00:59:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ def update
   DB[:conn].execute(sql, self.name, self.grade, self.id)
 end
 def self.create(name,grade)
-  student = self.new(name,grade,id)
+  student = self.new(name,grade)
   student.name = name 
   student.grade = grade 
   student
